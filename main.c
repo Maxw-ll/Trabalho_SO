54daed40661ce0fb7c3860e264f99058b9f1b331
Max <maxwellpires2002@gmail.com>
2024-05-30 10:30:02 -0300

    Att Quase Completas

--- main.c
+++ main.c
@@ -13,9 +13,10 @@
 //Tamanho Máximo do Disco
 #define TMAX_DISCO 1048576 // 1 MB
 
+//Supoorta Arquivos com nome menores que 100 Caracteres e com Conteudo de até 10 mil Carcateres
 #define TMAX_NAME 100
 #define TMAX_COMAND 4
-#define TMAX_CONTENT 500
+#define TMAX_CONTENT 10000 
 
 typedef struct {
     char name [TMAX_NAME]; //Nome do Arquivo
@@ -52,7 +53,7 @@ void load_Diretorio() {
     fclose(disk);
 }
 
-void save_Diretorio() {
+void salvar_diretorio() {
     FILE *disk = fopen(DISK_NAME, "r+b");
     if (!disk) {
         perror("Erro ao Abrir o Disco!");
@@ -72,7 +73,7 @@ int verifica_existencia(char *name){
     return FALSE;
 }
 
-void create_file(char *name, char *content) {
+void criar_arquivo(char *name, char *content) {
     if (verifica_existencia(name) == TRUE){
         printf("Nome de Arquivo ja Existe! Insira um Nome Diferente!\n");
         return;
@@ -82,8 +83,13 @@ void create_file(char *name, char *content) {
         return;
     }
 
+    int start;
     int content_len = strlen(content);
-    int start = (file_count == 0) ? sizeof(int) + MAXQTD_FILES * sizeof(Arquivo) : Diretorio[file_count - 1].start + Diretorio[file_count - 1].size;
+    if(file_count == 0){
+        start = sizeof(int) + MAXQTD_FILES * sizeof(Arquivo); 
+    } else{
+        start = Diretorio[file_count - 1].start + Diretorio[file_count - 1].size;
+    }
 
     if (start + content_len > TMAX_DISCO) {
         printf("O Disco está Cheio! Sem Espaço Suficiente!\n");
@@ -100,10 +106,10 @@ void create_file(char *name, char *content) {
     fwrite(content, sizeof(char), content_len, disk);
     fclose(disk);
 
-    save_Diretorio();
+    salvar_diretorio();
 }
 
-void list_files() {
+void listar_arquivos() {
     for (int i = 0; i < file_count; i++) {
         printf("%s\n", Diretorio[i].name);
     }
@@ -132,14 +138,11 @@ void mostrar_arquivo(char *name) {
 
 //Definir a função do Comando quando temos mais de duas entradas no terminal
 void tratar_comando(char* comando, char* arquivo, char* conteudo){
-
     if (strcmp(comando, "cat") == 0) {
-        create_file(arquivo, conteudo);
+        criar_arquivo(arquivo, conteudo);
     } else if (strcmp(comando, "more") == 0) {
         mostrar_arquivo(arquivo);
-    } else {
-        printf("Comando Desconhecido!: Digite /help para visualizar os comandos disponiveis\n");
-    }
+    } 
 }
 
 //Imprimir os Comandos Disponíveis
@@ -152,26 +155,56 @@ void imprimir_help(){
     printf("[Encerrar]        : exit\n");
 }
 
+
 // Impressão para Simular um Terminal identificando o comando digitado e realizando sua função
 int mini_terminal(char* comando, char* arquivo, char* conteudo){
+    
+    //"Apagar" o conteúdo das entradas para reutilizálas de novo
+    //Não realmente apagamos, apenas como o '\0' no inicio, pois '\0' demarca o fim da string
+    comando [0] = '\0';
+    arquivo [0] = '\0';
+    conteudo[0] = '\0';
+
     printf("M:\\Max\\");
     scanf("%s", comando);
+    int get_next_entry = FALSE;
+
+    //Como são strings não conseguimos usar Switch, else if mesmo. Percorrendo todos os possíveis comandos. 
+    //Existem comandos de uma linha só, como ls, /help, clear, exit. Esses já sao executados aqui
+    //Mas ainda temos o cat e more que precisam de mais argumentos. Nesse caso, pegamos as próximas entradas
+    //Caso nenhum dos comandos 
     if (strcmp(comando, "ls") == 0){
-        list_files();
+        listar_arquivos();
     } else if (strcmp(comando, "/help") == 0){
         imprimir_help();
     } else if (strcmp(comando, "exit") == 0){
         return FALSE;
     } else if (strcmp(comando, "clear") == 0){
         system("cls");
-    } else if (strlen(arquivo) == 0){
+    } else if (strcmp(comando, "cat") == 0) {
+        get_next_entry = TRUE;
+    } else if (strcmp(comando, "more") == 0) {
+        get_next_entry = TRUE;
+    } else {
         printf("Comando Desconhecido!: Digite /help para visualizar os comandos disponiveis\n");
+        //Aqui o fgets pega todo o restante digitado para Apaga-lo depois, impedindo que o comando pegar cada entrada separada. 
+        //Caso contrario tinhamos um bug de imprimir essa mensagem varias vezes, a quantidade de quantas entradas separadas por espaço tivermos
+        fgets(conteudo, TMAX_CONTENT, stdin);
     }
-    else{
+    
+    if (get_next_entry == TRUE){
         scanf("%s", arquivo);
+        if (strcmp(comando, "cat") == 0){
+            getchar(); // Pega o próximo Espaço na Entrada. Para no conteudo ficar sem esse espaço Inicial
+        }
         fgets(conteudo, TMAX_CONTENT, stdin);
         tratar_comando(comando, arquivo, conteudo);
+    } else{
+        //Limpar e entrada quando digitado um comando que já existe como ls e o usuario digita algo a mais. Ex: ls arquivo conteudo
+        //Caso contrario tinhamos um bug de imprimir essa mensagem varias vezes, a quantidade de quantas entradas separadas por espaço tivermos
+        fgets(conteudo, TMAX_CONTENT, stdin);
     }
+    
     return TRUE; 
 }
 
@@ -201,7 +234,7 @@ int main(int argc, char *argv[]) {
         run = mini_terminal(comando, arquivo, conteudo);
     }
 
-    printf("Obrigado por usar meu Programa de Gerenciamento de Arquvos em Disco :)");
+    printf("Obrigado por usar meu Programa de Gerenciamento de Arquivos em Disco!");
     return 0;
 }
 
